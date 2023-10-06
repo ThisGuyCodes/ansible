2dc97396f6ed504f45dfe086e1f7dade5c4f5e6c
Travis Johnson <travis@thisguy.codes>
2023-10-06 14:35:27 -0400

    run script

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -euo pipefail
+
+declare -r SCRIPT_DIR=$( cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+cd "$SCRIPT_DIR"
+
+exec ansible localhost -m include_role -a name=workstation
