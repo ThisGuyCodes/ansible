39357021814107b2678745ba913d7e1c2a6602e3
Travis Johnson <travis@thisguy.codes>
2023-10-24 11:41:52 -0400

    sync before run

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-declare -r SCRIPT_DIR=$( cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+declare -r SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 cd "$SCRIPT_DIR"
+
+pipenv sync
 
 # exec ansible localhost -m include_role -a name=workstation "$@"
 exec pipenv run ansible-playbook ./play.yml "$@"
